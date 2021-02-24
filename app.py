0ef9c094e1f6500d2662b0509590528fa697ab35
zerrium <zerrium@mail.com>
2021-02-24 17:53:13 +0700

    Added front end design from BootStrap Studio

--- app.py
+++ app.py
@@ -3,30 +3,23 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    list = {
-        '/anggota': 'Anggota Kelompok',
-        '/foto': 'Foto',
-        '/css': 'CSS',
-    }
-    return render_template('index.html', list=list)
+    return render_template('index.html')
 
-@application.route('/anggota')
-def anggota():
-    listAnggota = [
-        'Aryo',
-        'Willy',
-        'Randinesia',
-        'Fulo',
-    ]
-    return render_template('listanggota.html', listAnggota=listAnggota)
+@application.route('/about')
+def about():
+    return render_template('about.html')
 
-@application.route('/foto')
-def foto():
-    return render_template('foto.html')
+@application.route('/prime_odd')
+def prime_odd():
+    return render_template('prime_odd.html')
 
-@application.route('/css')
-def css():
-    return render_template('css.html')
+@application.route('/2d_shapes')
+def twod_shapes():
+    return render_template('2d_shapes.html')
+
+@application.route('/3d_shapes')
+def threed_shapes():
+    return render_template('3d_shapes.html')
 
 if __name__ == '__main__':
     application.run(debug=True)

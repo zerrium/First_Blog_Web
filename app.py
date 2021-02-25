9d9b6f63d40b2ced0526648e3738fd06bdc2934b
zerrium <zerrium@mail.com>
2021-02-25 12:05:23 +0700

    Finished 3d shapes backend python

--- app.py
+++ app.py
@@ -86,6 +86,42 @@ def twod_shapes():
 
 @application.route('/3d_shapes', methods=['POST', 'GET'])
 def threed_shapes():
+    input2 = 0
+    input3 = 0
+    if request.method == 'POST':
+        shape = request.form['shape']
+        scroll = int(request.form['scroll']) + 20
+        # Menggunakan exception handling python untuk menangani input dari user yang bukan angka
+        try:
+            input1 = int(request.form['input1'])  # Typecast dari string dari input text box ke integer
+            if shape == "cone" or shape == "cube" or shape == "cylinder":
+                input2 = int(request.form['input2'])
+            if shape == "cube":
+                input3 = int(request.form['input3'])
+        except ValueError:
+            error = "Only number inputs are allowed!"  # Ketika user menginput bukan angka
+            return render_template('3d_shapes.html', error=error, scroll=scroll)
+        else:
+            output = [shape, input1, input2, input3]  # variabel list
+
+            if shape == "cylinder":
+                output.append(pi * input1 ** 2 * input2)  # Volume tabung
+                output.append(2 * pi * input1 * input2 + 2 * pi * input1 ** 2)  # Luas permukaan tabung
+            elif shape == "cone":
+                output.append(1/3 * pi * input1 ** 2 * input2)  # Volume kerucut
+                output.append(pi * input1 * (input1 + (input2**2 + input1**2)**0.5))  # Luas permukaan kerucut
+            elif shape == "cube":
+                output.append(input1 * input2 * input3)  # Volume balok/kubus
+                output.append(2 * (input1 + input2) + 2 * (input1 + input3) + 2 * (input2 + input3))  # Luas permukaan balok/kubus
+            elif shape == "sphere":
+                output.append(4/3 * pi * input1 ** 3)  # Volume bola
+                output.append(4 * pi * input1 ** 2)  # Luas permukaan bola
+            else:
+                error = "Please select a shape!"  # Ketika user tidak memilih shape
+                return render_template('3d_shapes.html', error=error, scroll=scroll)
+
+            return render_template('3d_shapes.html', output=output, scroll=scroll)
+
     return render_template('3d_shapes.html')
 
 
